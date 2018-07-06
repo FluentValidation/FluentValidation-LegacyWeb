555b3d3f1a5d38ca5f6658c81a03b3a4416084a4
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2018-07-06 20:17:18 +0100

    Deprecate IParameterValidatorFactory

---
+++ src/FluentValidation.ValidatorAttribute/IParameterValidatorFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace FluentValidation
+{
+	using System;
+	using System.Reflection;
+
+	/// <summary>
+	/// Gets validators for method parameters.
+	/// </summary>
+	[Obsolete]
+	public interface IParameterValidatorFactory {
+		/// <summary>
+		/// Gets a validator for <paramref name="parameterInfo"/>.
+		/// </summary>
+		/// <param name="parameterInfo">The <see cref="ParameterInfo"/> instance to get a validator for.</param>
+		/// <returns>Created <see cref="IValidator"/> instance; <see langword="null"/> if a validator cannot be
+		/// created.</returns>
+		IValidator GetValidator(ParameterInfo parameterInfo);
+	}
+}
